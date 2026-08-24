d2a336c2a9406ac301b57e838cf4615bd314f7fe
jose <jwdevelopper@gmail.com>
2026-08-24 20:52:12 -0300

    Implementação de Card e responsividade dos campos nome e tipo de pessoa

--- src/app/components/cliente/cliente-cadastrar/cliente-cadastrar.ts
+++ src/app/components/cliente/cliente-cadastrar/cliente-cadastrar.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { CardModule } from 'primeng/card';
+import { SelectModule } from 'primeng/select';
 
 @Component({
   selector: 'app-cliente-cadastrar',
-  imports: [InputTextModule, FloatLabelModule],
+  imports: [
+    InputTextModule,
+    FloatLabelModule,
+    CardModule,
+    SelectModule
+],
   templateUrl: './cliente-cadastrar.html',
   styleUrl: './cliente-cadastrar.css',
 })
-export class ClienteCadastrar {}
+export class ClienteCadastrar {
+
+  tipoPessoaOptions: any[] = [
+    { descricao: 'Pessoa Física', valor: 'PF'},
+    { descricao: 'Pessoa Jurídica', valor: 'PJ'}
+  ];
+}
